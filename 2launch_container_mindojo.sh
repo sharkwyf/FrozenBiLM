a921caddf95f41801d43909e1597f3907ff4e540
wangyuanfu <wangyuanfu@pjlab.org.cn>
2022-10-17 18:24:43 +0800

    Update mineclip feature extraction script

---
+++ 2launch_container_mindojo.sh
@@ -0,0 +1,7 @@
+
+docker run --gpus all --ipc=host --rm -itd \
+    --mount src=$(pwd),dst=/FrozenBiLM,type=bind \
+    --env DATA_DIR="/FrozenBiLM/data" \
+    --env TRANSFORMERS_CACHE="/FrozenBiLM/transformers_cache" \
+    -w /FrozenBiLM sharkwyf/mineclip:latest \
+    bash -c "unset HTTP_PROXY; unset HTTPS_PROXY; bash" 
--- extract/extract_mineclip_features.py
+++ extract/extract_mineclip_features.py
@@ -6,6 +6,9 @@ import numpy as np
 from io import StringIO, BytesIO
 import kornia
 import torch.nn.functional as F
+import torch.distributed as dist
+import torch.multiprocessing as mp
+from torch.nn.parallel import DistributedDataParallel as DDP
 from tqdm import tqdm
 import argparse
 from mineclip import MineCLIP
@@ -18,38 +21,17 @@ MC_IMAGE_STD = (0.2439, 0.2493, 0.2873)
 Transfer video frames into Vit features
 then, split captions into past, current, future sentences
 """
-
-parser = argparse.ArgumentParser(description="Easy video feature extractor")
-parser.add_argument("--indexfile", default="../data/Minedojo/youtube_full.json", type=str)
-parser.add_argument("--trans", default="s3://minedojo/trans/v1/", type=str)
-parser.add_argument("--outputfile", default="../data/Minedojo/mineclip_features.npy", type=str)
-parser.add_argument("--cluster", default="cluster1", type=str)
-parser.add_argument("--ceph", default=True, type=bool)
-parser.add_argument("--half_precision", type=int, default=1, help="whether to output half precision float or not")
-args = parser.parse_args()
-if args.ceph:
-    from petrel_client.client import Client
-    client = Client()
-
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-indexfile = os.path.join(os.path.dirname(__file__), args.indexfile)
-outputfile = os.path.join(os.path.dirname(__file__), args.outputfile)
-# with open(indexfile) as f:
-#     youtube_dataset = json.load(f)
-
-files = client.get_file_iterator(f"{args.cluster}:{args.trans}")
-clip_param = {
-    "arch": "vit_base_p16_fz.v2.t2",
-    "hidden_dim": 512,
-    "image_feature_dim": 512,
-    "mlp_adapter_spec": "v0-2.t0",
-    "pool_type": "attn.d2.nh8.glusw",
-    "resolution": [160, 256],
-}
-model = MineCLIP(**clip_param).to(device)
-model.load_ckpt(os.path.expanduser(os.path.join(os.path.dirname(__file__), "..", "data", "Minedojo", "attn.pth")), strict=True)
-
+def get_args():
+    parser = argparse.ArgumentParser(description="Easy video feature extractor")
+    parser.add_argument("--indexfile", default="../data/Minedojo/youtube_full.json", type=str)
+    parser.add_argument("--trans", default="s3://minedojo/trans/v1/arrow/", type=str)
+    parser.add_argument("--outputfile", default="../data/Minedojo/mineclip_features.npy", type=str)
+    parser.add_argument("--cluster", default="cluster1", type=str)
+    parser.add_argument("--ceph", default=True, type=bool)
+    parser.add_argument("--world_size", default=8, type=int)
+    parser.add_argument("--half_precision", type=int, default=1, help="whether to output half precision float or not")
+    args = parser.parse_args()
+    return args
 
 def torch_normalize(tensor: torch.Tensor, mean, std, inplace=False):
     """
@@ -94,16 +76,38 @@ def torch_normalize(tensor: torch.Tensor, mean, std, inplace=False):
 def resize_frames(frames, resolution):
     return kornia.geometry.transform.resize(frames, resolution).clamp(0.0, 255.0)
 
-with tqdm(total=100000) as pbar:
+def run_extract_features(rank, world_size, args, contents, mp_lock):
+    dist.init_process_group("nccl", rank=rank, world_size=world_size)
+    torch.cuda.set_device(rank)
+    device = torch.device("cuda", rank) if torch.cuda.is_available() else "cpu"
+
+    # indexfile = os.path.join(os.path.dirname(__file__), args.indexfile)
+    # with open(indexfile) as f:
+    #     youtube_dataset = json.load(f)
+    outputfile = os.path.join(os.path.dirname(__file__), args.outputfile)
+
+    clip_param = {
+        "arch": "vit_base_p16_fz.v2.t2",
+        "hidden_dim": 512,
+        "image_feature_dim": 512,
+        "mlp_adapter_spec": "v0-2.t0",
+        "pool_type": "attn.d2.nh8.glusw",
+        "resolution": [160, 256],
+    }
+    model = MineCLIP(**clip_param).to(device)
+    model.load_ckpt(os.path.expanduser(os.path.join(os.path.dirname(__file__), "..", "data", "Minedojo", "attn.pth")), strict=True)
+
+    if args.ceph:
+        from petrel_client.client import Client
+        client = Client()
+    
+    pbar = tqdm(total=len(contents[rank::world_size]))
     with torch.no_grad():
         output_features = {}
-        for p, k in files:
-            print(p)
-            if not p.endswith(".npy"):
-                continue
+        for i, p in enumerate(contents[rank::world_size]):
             # Extract video features
-            stream = client.get(f"s3://{p}")   # enable_stream=True for large data
-            frames = np.load(BytesIO(stream), encoding="bytes")
+            stream = BytesIO(client.get(f"{args.trans}{p}.npy"))   # enable_stream=True for large data
+            frames = np.load(stream, encoding="bytes")
             # 64, H, W, C -> 16, C, H, W
             frames = torch.tensor(frames[::4].transpose(0, 3, 1, 2), dtype=float, device=device)
             frames = resize_frames(frames, clip_param["resolution"])
@@ -113,9 +117,8 @@ with tqdm(total=100000) as pbar:
                 features = features.astype("float16")
 
             # Extract captions
-            stream = client.get(f"s3://{p[:-4]}.txt")   # enable_stream=True for large data
-            # frames = np.load(BytesIO(stream), encoding="bytes")
-            text = BytesIO(stream).read().decode("utf-8")
+            stream = BytesIO(client.get(f"{args.trans}{p}.txt"))  # enable_stream=True for large data
+            text = stream.read().decode("utf-8")
             splits = [item.split("|") for item in text.replace(" ", "").split("\n")]
             err = [x for x in splits[:-1] if len(x) != 3]
             if len(err) > 0:
@@ -127,9 +130,32 @@ with tqdm(total=100000) as pbar:
                 "word": np.array([x[2]for x in splits]),
             }
 
-            output_features[p.split("/")[-1][:-4]] = [features, captions]
-            pbar.update(1)
-            break
+            output_features[p] = [features, captions]
+            with mp_lock:
+                pbar.update(1)
         np.save(outputfile, output_features)
 
         print(f"Extraction completed, saved to {outputfile}")
+
+def main():
+    # mp_array = mp.Array("i", [0 for _ in range(cfg.world_size)])
+    mp_lock = mp.Lock()
+    args = get_args()
+    
+    if args.ceph:
+        from petrel_client.client import Client
+        client = Client()
+    contents = list(client.list(f"{args.cluster}:{args.trans}"))
+    contents = [x[:-4] for x in contents if x.endswith(".npy")]
+    mp.spawn(run_extract_features,
+        args=(args.world_size, args, contents, mp_lock),
+        nprocs=args.world_size,
+        join=True)
+
+if __name__ == "__main__":
+    # os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+    os.environ["CUDA_LAUNCH_BLOCKING"] = '1'
+    os.environ["MASTER_ADDR"] = "localhost"
+    os.environ["MASTER_PORT"] = str(49500)
+    mp.set_start_method('spawn', force = True)
+    main()
